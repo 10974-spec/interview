37b4597799751299e6123a967a24f9a95fc277bc
10974-spec <emmanuelnyakoe13@gmail.com>
2025-06-24 00:34:16 +0300

    Profile Info card

--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -3,6 +3,11 @@ import { useNavigate } from 'react-router-dom';
 import Input from '../../components/Inputs/Input';
 import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector';
 import { validateEmail } from '../../utils/helper';
+import { useContext } from 'react';
+import { UserContext } from '../../context/userContext';
+import axiosInstance from '../../utils/axiosinstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import uploadImage from '../../utils/uploadImage';
 
 const SignUp = ({setCurrentPage}) => {
   const [profilePic, setProfilePic] = useState(null);
@@ -12,7 +17,7 @@ const SignUp = ({setCurrentPage}) => {
 
 
   const [error, setError] = useState(null);
-
+  const { updateUser} = useContext(UserContext);
   const navigate = useNavigate();
 
 
@@ -42,7 +47,27 @@ const SignUp = ({setCurrentPage}) => {
 
     // *****SIGNUP API CALL*****//  
 
-      try{
+    try{
+      //Upload Image if present 
+      if (profilePic){
+        const imgUploadRes = await uploadImage(profilePic);
+        profileImageUrl = imgUploadRes.imageUrl;
+      }
+
+      const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER,{
+        name: fullName,
+        email,
+        password,
+        profileImageUrl
+      });
+
+      const { token } = response.data;
+
+      if (token){
+        localStorage.setItem("token", token);
+        updateUser(response.data)
+        navigate("/dashboard");
+      }
 
     }catch(error){
       if(error.response && error.response.data.message){
--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -7,7 +7,7 @@ export const API_PATHS = {
         GET_PROFILE: "/api/auth/profile",
     },
     IMAGE: {
-        UPLOAD_IMAGE: "/api/image/upload-image",
+        UPLOAD_IMAGE: "/api/auth/upload-image",
     },
 
     AI:{
