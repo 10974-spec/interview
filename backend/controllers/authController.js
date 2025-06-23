69189b99f1e73f6c28de8642bd0ae940ecaecc68
10974-spec <emmanuelnyakoe13@gmail.com>
2025-06-23 10:49:48 +0300

    register user controller function

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -14,7 +14,35 @@ const generateToken = (id) => {
 // @access Public
 
 const registerUser = async (req, res) => {
-   
+   try {
+    const {name, email, password, profileImageurl} = req.body;
+    //****Check if user exists****//
+    const userExists = await User.findOne({email});
+    if(userExists){
+        res.status(400).json({message:"User already exists"});
+    }
+    //****HASH PASSWORD****/
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    //****CREATE USER****//
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+        profileImageurl
+    });
+    //****return user data with jwt****/
+    res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        profileImageurl: user.profileImageurl,
+        token: generateToken(user._id)
+    })
+    
+   }catch(error){
+    res.status(500).json({message:error.message});
+   }
 };
 
 // ****LOGIN USER****//
