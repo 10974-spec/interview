7654fd507f0f0226a259de49edf47795bc076cf1
10974-spec <emmanuelnyakoe13@gmail.com>
2025-06-23 11:45:24 +0300

    login controller function

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 // ****GENERATE JWT TOKEN****//
 
 const generateToken = (id) => {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 };
 
 // ****REGISTER USER****//
@@ -14,35 +14,34 @@ const generateToken = (id) => {
 // @access Public
 
 const registerUser = async (req, res) => {
-   try {
-    const {name, email, password, profileImageurl} = req.body;
+  try {
+    const { name, email, password, profileImageUrl } = req.body;
     //****Check if user exists****//
-    const userExists = await User.findOne({email});
-    if(userExists){
-        res.status(400).json({message:"User already exists"});
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      res.status(400).json({ message: "User already exists" });
     }
     //****HASH PASSWORD****/
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
     //****CREATE USER****//
     const user = await User.create({
-        name,
-        email,
-        password: hashedPassword,
-        profileImageurl
+      name,
+      email,
+      password: hashedPassword,
+      profileImageUrl,
     });
     //****return user data with jwt****/
     res.status(201).json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        profileImageurl: user.profileImageurl,
-        token: generateToken(user._id)
-    })
-    
-   }catch(error){
-    res.status(500).json({message:error.message});
-   }
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      profileImageUrl: user.profileImageUrl,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
 };
 
 // ****LOGIN USER****//
@@ -50,9 +49,30 @@ const registerUser = async (req, res) => {
 // @route POST/api/auth/login
 // @access Public
 
-
 const loginUser = async (req, res) => {
-   
+  try {
+    const { email, password } = req.body;
+    //****Check if user exists****//
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400).json({ message: "User does not exist" });
+    }
+    //****CHECK PASSWORD****//
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      res.status(500).json({ message: "Invalid password email or password" });
+    }
+    //****return user data with jwt****/
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      profileImageUrl: user.profileImageUrl,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
 };
 
 // ****GET USER PROFILE****//
@@ -61,11 +81,14 @@ const loginUser = async (req, res) => {
 // @access Private
 
 const getUserProfile = async (req, res) => {
-
-}
+  try {
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
 
 module.exports = {
-    registerUser,
-    loginUser,
-    getUserProfile
-}
+  registerUser,
+  loginUser,
+  getUserProfile,
+};
