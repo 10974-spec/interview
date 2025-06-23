b4558acdb2d618b95a4600f182049e14b2c5dab3
10974-spec <emmanuelnyakoe13@gmail.com>
2025-06-23 12:08:23 +0300

    backend upload middleware

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -82,6 +82,12 @@ const loginUser = async (req, res) => {
 
 const getUserProfile = async (req, res) => {
   try {
+    const user = await User.findById(req.user._id).select("-password");
+    if (!user) {
+      res.status(404).json({ message: "User does not exist" });
+    }
+    res.status(200).json(user);
+      
   } catch (error) {
     res.status(500).json({ message: "Server Error", error: error.message });
   }
