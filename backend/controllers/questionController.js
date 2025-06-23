9dca52f41d0df18eca6756659c1128f0662a6b57
10974-spec <emmanuelnyakoe13@gmail.com>
2025-06-23 20:04:28 +0300

    question controller functions

--- backend/controllers/questionController.js
+++ backend/controllers/questionController.js
@@ -7,6 +7,31 @@ const Session = require("../models/Session");
 
 exports.addQuestionsToSession = async(req,res) =>{
     try{
+        const {sessionId,questions} = req.body;
+
+        if(!sessionId || !questions || !Array.isArray(questions)){
+            return res.status(400).json({success:false,message:"Invalid input Data"});
+        }
+        const session = await Session.findById(sessionId);
+
+        if(!session){
+            return res.status(404).json({success:false,message:"Session not found"});
+        }
+
+        //*****create new questions *****//
+        const createdQuestions = await Question.insertMany;
+
+            questions.map((q) =>({
+                session: sessionId,
+                question: q.question,
+                answer: q.answer,
+            }));
+
+         //***UPDATE SESSION TO INCLUDE NEW QUESTION IDS ***//   
+            session.questions.push(...createdQuestions.map((q) => q._id));
+            await session.save();
+
+            res.status(201).json({createdQuestions});
 
     }catch(error){
         res.status(500).json({success:false,message:"Server Error"});
@@ -19,6 +44,18 @@ exports.addQuestionsToSession = async(req,res) =>{
 
 exports.togglePinQuestion = async(req,res) =>{
     try{
+        const question = await Question.findById(req.params.id);
+
+        if(!question){
+            return res.status(404).json({success:false,message:"Question not found"});
+        }
+
+        question.isPinned = !question.isPinned;
+        await question.save();
+
+        res.status(200).json({success:true,message:"Question pinned state updated",question});
+
+
     }catch(error){
         res.status(500).json({success:false,message:"Server Error"});
     }
@@ -30,6 +67,17 @@ exports.togglePinQuestion = async(req,res) =>{
 
 exports.updateQuestionNote = async(req,res) =>{
     try{
+        const { note } = req.body;
+        const question = await Question.findById(req.params.id);
+
+        if(!question){
+            return res.status(404).json({success:false,message:"Question not found"});
+        }
+
+        question.note = note;
+        await question.save();
+
+        res.status(200).json({success:true,message:"Question note updated",question});
     }catch(error){
         res.status(500).json({success:false,message:"Server Error"});
     }
