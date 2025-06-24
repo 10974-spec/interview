3440035bd85c1651135c4cc91fbc115493381ec7
10974-spec <emmanuelnyakoe13@gmail.com>
2025-06-24 22:45:22 +0300

    interview prep

--- frontend/src/pages/InterviewPrep/interviewPrep.jsx
+++ frontend/src/pages/InterviewPrep/interviewPrep.jsx
@@ -1,8 +1,15 @@
+import moment from 'moment'
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import {AnimatePresence, motion} from 'framer-motion'
+import { LuCircleAlert, LuListCollapse } from 'react-icons/lu'
+import SpinnerLoader from '../../components/Loader/SpinnerLoader'
+
 
 const interviewPrep = () => {
   return (
-    <div>interviewPrep</div>
+    <div></div>
+
   )
 }
 
