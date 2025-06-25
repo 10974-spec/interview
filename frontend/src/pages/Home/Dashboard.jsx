52b26c33df07d021688abd179b8105e135d1f3bd
10974-spec <emmanuelnyakoe13@gmail.com>
2025-06-25 15:57:25 +0300

    done

--- frontend/src/pages/Home/Dashboard.jsx
+++ frontend/src/pages/Home/Dashboard.jsx
@@ -10,6 +10,7 @@ import SummaryCard from "../../components/Cards/SummaryCard";
 import moment from "moment";
 import Modal from "../../components/Modal";
 import CreateSessionForm from "./CreateSessionForm";
+import DeleteAlertContent from "../../components/DeleteAlertContent";
 
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -31,7 +32,17 @@ const Dashboard = () => {
     }
   };
 
-  const deleteSession = async (sessionData) => {};
+  const deleteSession = async (sessionData) => {
+    try {
+
+      await axiosInstance.delete(API_PATHS.SESSION.DELETE(sessionData?._id));
+      setOpenDeleteAlert({ open: false, data: null });
+      toast.success("Session deleted successfully");
+      fetchAllSessions();
+    }catch(error){
+      toast.error("Error deleting session");
+    }
+  };
 
   useEffect(() => {
     fetchAllSessions();
@@ -79,6 +90,21 @@ const Dashboard = () => {
   <div>
     <CreateSessionForm/>
   </div>
+</Modal>
+
+<Modal
+ isOpen={openDeleteAlert?.open}
+ onClose={() => {
+  setOpenDeleteAlert({ open: false, data: null });
+ }}
+ title="Delete Alert"
+>
+<div className="w-[30vw]">
+  <DeleteAlertContent
+   content="Are you sure you want to delete this session details?"
+   onDelete={() => deleteSession(openDeleteAlert?.data)}
+   />
+</div>
 </Modal>
 
     </DashboardLayout>
